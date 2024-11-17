5f5c1fac9ce1782a7bb0c18dfbc9eb75ad0e0047
akemilol <valeriaconcedsantos@gmail.com>
2024-11-16 21:21:43 -0300

    Criação do componente de Menu e adição dos comentarios

--- e-lab/src/app/login/page.tsx
+++ e-lab/src/app/login/page.tsx
@@ -1,8 +1,11 @@
+import MenuLogin from "@/components/MenuLogin/MenuLogin";
 import Voltar from "@/components/Voltar/Voltar";
+
 
 export default function Login() {
     return (
         <div>
+            <MenuLogin />
             <Voltar />
         </div>
     );

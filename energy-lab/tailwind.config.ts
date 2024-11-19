89e2e9e1b6e3d904129242feed1ec0af7f726712
akemilol <valeriaconcedsantos@gmail.com>
2024-11-19 17:25:56 -0300

    Atualização da versão do next.js

--- energy-lab/tailwind.config.ts
+++ energy-lab/tailwind.config.ts
@@ -1,6 +1,6 @@
 import type { Config } from "tailwindcss";
 
-export default {
+const config: Config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -8,9 +8,6 @@ export default {
   ],
   theme: {
     extend: {
-      maxWidth: {
-        '113rem': '113rem',
-      },
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
@@ -18,4 +15,5 @@ export default {
     },
   },
   plugins: [],
-} satisfies Config;
+};
+export default config;

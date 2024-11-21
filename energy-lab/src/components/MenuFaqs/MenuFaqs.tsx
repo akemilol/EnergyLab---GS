2be275722069b343068ea5fd8ede2ee25f8806f2
mirelapsr <mirelarodrigues05@gmail.com>
2024-11-21 19:45:21 -0300

    Ajustando o MenuFaqs

--- energy-lab/src/components/MenuFaqs/MenuFaqs.tsx
+++ energy-lab/src/components/MenuFaqs/MenuFaqs.tsx
@@ -20,28 +20,26 @@ return (
         <h1 className="text-2xl font-bold mb-6">FAQs – EnergyLab</h1>
 
         <div className="bg-gray-800 p-6 rounded-lg">
-            <h2 className="text-lg font-semibold">Lorem ipsum</h2>
+            <h2 className="text-lg font-semibold">  O que é a EnergyLab? </h2>
             <p className="text-gray-400 mt-2">
-            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been
-            the industry's standard dummy text ever since the 1500s.
+            A EnergyLab é uma plataforma digital que ajuda pessoas e empresas a entenderem e otimizarem seu consumo energético. Por meio de gráficos interativos, relatórios detalhados e análises personalizadas, a EnergyLab promove eficiência energética e sustentabilidade, ajudando você a economizar e reduzir seu impacto ambiental.
             </p>
         </div>
 
         <div className="bg-gray-800 p-6 rounded-lg">
-            <h2 className="text-lg font-semibold">Lorem ipsum</h2>
+            <h2 className="text-lg font-semibold"> Como a EnergyLab ajuda a reduzir meu consumo de energia? </h2>
             <p className="text-gray-400 mt-2">
-            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been
-            the industry's standard dummy text ever since the 1500s.
+            Analisamos seu consumo energético com base nos dados fornecidos e identifica desperdícios ou oportunidades de economia. Além disso, oferece relatórios detalhados e notificações inteligentes para sugerir mudanças práticas que otimizam seu uso de energia, resultando em economia e menor impacto ambiental.
             </p>
         </div>
 
         <div className="bg-gray-800 p-6 rounded-lg">
-            <h2 className="text-lg font-semibold">Lorem ipsum</h2>
+            <h2 className="text-lg font-semibold"> Quem pode usar a EnergyLab? </h2>
             <p className="text-gray-400 mt-2">
-            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been
-            the industry's standard dummy text ever since the 1500s.
+            A plataforma é ideal para residências, pequenas empresas, grandes indústrias e qualquer pessoa ou organização que deseja melhorar sua eficiência energética. A EnergyLab adapta-se às suas necessidades, oferecendo planos acessíveis e funcionalidades personalizadas para diferentes perfis de usuários.
             </p>
         </div>
+
         </div>
 
     </div>

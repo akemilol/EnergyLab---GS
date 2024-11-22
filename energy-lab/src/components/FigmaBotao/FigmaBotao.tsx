5efb567e98cb723dcc81bf6b2a8f6cd286e9d170
JPAmorimBV <jpamorim2101@gmail.com>
2024-11-22 01:27:01 -0300

    adição de comentarios e links

--- energy-lab/src/components/FigmaBotao/FigmaBotao.tsx
+++ energy-lab/src/components/FigmaBotao/FigmaBotao.tsx
@@ -6,11 +6,14 @@ import { FaFigma } from 'react-icons/fa';
 export default function FigmaButton() {
 return (
     <div className="flex justify-center items-start ">
-    <button
-        onClick={() => window.location.href = "#"} 
-        className="flex items-center space-x-2 px-4 py-3 rounded-lg text-black bg-zinc-300 shadow-md hover:bg-zinc-400 transition duration-300">
-        <FaFigma size={24} className="text-[#F24E1E]" />
-        <span className="font-medium">Acesso ao repositorio</span>
+
+    {/* Botão de acesso ao repositorio do Figma */}
+<button
+    onClick={() => window.open("https://www.figma.com/design/a7L7lumVQDi9Nm2wByIBKz/EnergyLab?node-id=1-2&t=LDuOjufsuskx0eMN-1", "_blank")}
+    className="flex items-center space-x-2 px-4 py-3 rounded-lg text-black bg-zinc-300 shadow-md hover:bg-zinc-400 transition duration-300"
+    >
+    <FaFigma size={24} className="text-[#F24E1E]" />
+    <span className="font-medium">Acesso ao repositório</span>
     </button>
     </div>
 );
